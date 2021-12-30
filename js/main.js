303769cbb149ed8895e96346c332d5088a2124d4
Diego Luiz <51066402+Diego-Luiz@users.noreply.github.com>
2021-12-30 13:39:52 -0400

    singup carousel done, hover effects init

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 (
   function(){
-    
+    document.body.classList.add('js')
+    const navBtn = document.querySelector('.nav-btn')
+    const navbar = document.getElementById('main-nav')
+    navBtn.addEventListener('click', toggleNavbar)
+    toggleNavbar()
+    function toggleNavbar(){
+      let expanded = navbar.classList.contains('--active') ? false : true
+      navBtn.setAttribute('aria-expanded', expanded)
+      navbar.classList.toggle('--active')
+    }
   }
 )()
